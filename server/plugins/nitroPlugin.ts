cacae9a294cb59ef29fea925692b20df961993e4
qiyuor2 <48339849+QiYuOr2@users.noreply.github.com>
2022-11-30 11:32:40 +0800

    fix: remove

--- server/plugins/nitroPlugin.ts
+++ server/plugins/nitroPlugin.ts
@@ -1,13 +1,13 @@
-import path from 'path';
-import fs from 'fs';
+// import path from 'path';
+// import fs from 'fs';
 
-export default defineNitroPlugin((nitro) => {
-  nitro.hooks.hook('render:html', (html) => {
-    const __NUXT__ = html.bodyAppend[0].replace(/<\/?script>/g, '');
-    const __NUXTPath = `/_nuxt/append.${Date.now()}.js`;
+// export default defineNitroPlugin((nitro) => {
+//   nitro.hooks.hook('render:html', (html) => {
+//     const __NUXT__ = html.bodyAppend[0].replace(/<\/?script>/g, '');
+//     const __NUXTPath = `/_nuxt/append.${Date.now()}.js`;
 
-    fs.writeFileSync(path.join(process.cwd(), __NUXTPath), __NUXT__);
+//     fs.writeFileSync(path.join(process.cwd(), __NUXTPath), __NUXT__);
 
-    html.bodyAppend[0] = `<script type="module" src="${__NUXTPath}" crossorigin></script>`;
-  });
-});
+//     html.bodyAppend[0] = `<script type="module" src="${__NUXTPath}" crossorigin></script>`;
+//   });
+// });
