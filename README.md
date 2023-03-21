0595a38a4eec2ce93e468c61ca4013c5143c8eb7
qiyuor2 <48339849+QiYuOr2@users.noreply.github.com>
2023-03-21 17:39:18 +0800

    rss

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 搭建工具：[Astro](https://astro.build/), [Fect UI](https://www.fect-org.com/), [Vercel](https://vercel.com/)
 
 Nuxt.js 会在页面注入一段`window.__nuxt__ = ...`这段代码体积非常大，很影响加载时间，并且未找到合适的解决方案，因此更换了Astro
+
+
+## Astro 注意事项
+
+Markdown 的 frontmatter 中需要有`pubDate`、`title`、`description`才能通过`await pagesGlobToRssItems(import.meta.glob('./posts/**/*.md'))`解析创建 rss
--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,11 +1,18 @@
 import rss, { pagesGlobToRssItems } from '@astrojs/rss';
 
 export async function get() {
+  const posts = Object.values(import.meta.glob('./posts/**/*.md', { eager: true }))
+
+  const getContent = (post: any) => {
+    const content = (posts.find((item: any) => item.frontmatter.title === post.title) as any).compiledContent()
+    return { ...post, content }
+  }
+
   return rss({
     title: '@柒宇',
     description: '柒宇的个人博客',
     site: 'https://blog.qiyuor2.cn',
-    items: await pagesGlobToRssItems(import.meta.glob('./posts/**/*.md')),
+    items: (await pagesGlobToRssItems(import.meta.glob('./posts/**/*.md'))).map(getContent),
     customData: `<language>zh-CN</language>`,
   });
 }
