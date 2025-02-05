23e6d6b2e4e6b68a7d6e9c811defcdde5bd2795c
qiyuor2 <48339849+QiYuOr2@users.noreply.github.com>
2025-02-05 21:38:23 +0800

    fix: date

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -3,13 +3,13 @@ import { sortByDate, visibleFilter } from "../collections/posts";
 import { getCollection } from "astro:content";
 import { BASE_URL } from "../constants";
 
-export async function GET(ctx) {
+export async function GET() {
   const posts = await getCollection("posts");
 
   return rss({
     title: "@柒宇",
     description: "柒宇的个人博客",
-    site: ctx.site,
+    site: BASE_URL,
     items:  posts
       .filter(visibleFilter)
       .sort(sortByDate)
