8bf7046f11a31412d0fbf34ac07b46432d2b7fb3
qiyuor2 <48339849+QiYuOr2@users.noreply.github.com>
2025-06-14 20:01:59 +0800

    feat: mobile style

--- src/components/vue/PostCard.vue
+++ src/components/vue/PostCard.vue
@@ -14,7 +14,7 @@ defineProps<{
 
 <template>
   <a  :href="path" :target="isExternal ? '_blank' : '_self'" no-underline>
-    <li class="flex gap-2 py-2 px-8 my-2 mx--8 list-none cursor-pointer">
+    <li class="flex flex-col sm:flex-row gap-2 py-2 px-8 my-2 mx--8 list-none cursor-pointer">
       <div class="flex transition-all duration-300 text-[1.1rem] text-warm-gray-400 hover:text-warm-gray-900 dark:hover:text-warm-gray-50">
         <div>{{ title }}</div>
         <FluentArrowUpRight20Filled v-if="isExternal" class="text-[0.6rem] ml-[0.3rem] text-warm-gray-400 text-opacity-80" />
