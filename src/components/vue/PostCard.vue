45d7712a1e0f2d91be55c69dbf6cdd6344bdb4f7
qiyuor2 <48339849+QiYuOr2@users.noreply.github.com>
2025-06-22 19:02:15 +0800

    feat: 颜色调整

--- src/components/vue/PostCard.vue
+++ src/components/vue/PostCard.vue
@@ -15,11 +15,11 @@ defineProps<{
 <template>
   <a  :href="path" :target="isExternal ? '_blank' : '_self'" no-underline>
     <li class="flex flex-col sm:flex-row gap-2 py-2 px-8 my-2 mx--8 list-none cursor-pointer">
-      <div class="flex transition-all duration-300 text-[1.1rem] text-warm-gray-400 hover:text-warm-gray-900 dark:hover:text-warm-gray-50">
+      <div class="flex transition-all duration-300 text-[1.1rem] text-true-gray-500 dark:text-true-gray-400 hover:text-warm-gray-900 dark:hover:text-warm-gray-50">
         <div>{{ title }}</div>
-        <FluentArrowUpRight20Filled v-if="isExternal" class="text-[0.6rem] ml-[0.3rem] text-warm-gray-400 text-opacity-80" />
+        <FluentArrowUpRight20Filled v-if="isExternal" class="text-[0.6rem] ml-[0.3rem] text-true-gray-500 dark:text-true-gray-400 text-opacity-80" />
       </div>
-      <div class="flex items-center font-normal text-[0.8rem] leading-normal text-warm-gray-400 text-opacity-80">
+      <div class="flex items-center font-normal text-[0.8rem] leading-normal text-true-gray-500 dark:text-true-gray-400 text-opacity-80">
         <div>
           {{ date.split(' ')[0].split('/').slice(1).join('/') }}
         </div>
