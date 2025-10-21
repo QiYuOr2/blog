2dfc2de3232b181ac66c69ccfaf6098f85334547
qiyuor2 <48339849+QiYuOr2@users.noreply.github.com>
2025-10-21 16:32:10 +0800

    Update comments.tsx

--- apps/site/src/components/comments.tsx
+++ apps/site/src/components/comments.tsx
@@ -59,7 +59,7 @@ export default function Comments() {
           repoId="R_kgDOGlnN5w"
           category="Announcements"
           categoryId="DIC_kwDOGlnN584COx7K"
-          mapping="url"
+          mapping="pathname"
           reactions-enabled="1"
           emitMetadata="0"
           inputPosition="top"
